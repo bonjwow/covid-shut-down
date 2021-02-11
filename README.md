580e35aeb03554057b07a13a3f5bbb308ad67d28
Bongju <bonjwow@gmail.com>
2021-02-11 01:29:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## About The Project
+This project is one of course assignments of [INF2178: Experimental Design for Data Science](https://rohanalexander.com/inf2178.html) instructed by Professor Rohan Alexander. 
+
+## Requirments
+* R
+* tidyverse
+* ggplot2
+* kableExtra
+* opendatatoronto
+
+## Getting Started
+1. Clone this repository
+  ```sh
+  git clone https://github.com/bonjwow/building_evaluation.git
+  ```
+2. Run `00-import_business_licences.R` in the `scripts` folder to obtain the most updated data from Open Data Portal
+
+## License
+Distributed under the MIT License. See `LICENSE` for more information.
---
+++ scripts/00-import_business_licences.R
@@ -0,0 +1,26 @@
+#### Preamble ####
+# Purpose: Get the most updated Municipal Licensing and Standards data from Open Data Portal
+# Authors: Bongju, Morgaine, Najma, and Rachit 
+# Contacts: bongju.yoo@mail.utoronto.ca, morgaine.westin@mail.utoronto.ca, najma.osman@mail.utoronto.ca, and rachit.srivastava@mail.utoronto.ca 
+# Date: 9 February 2021
+# Pre-requisites: None
+
+
+#### Workspace set-up ####
+install.packages("opendatatoronto")
+library(opendatatoronto)
+library(tidyverse)
+
+#### Check the list ####
+listPckg <- list_packages()
+
+#### Get data ####
+all_data <-
+  opendatatoronto::search_packages("Business Licences and Permits") %>%
+  opendatatoronto::list_package_resources() %>%
+  filter(name == "Business licences data") %>%
+  select(id) %>%
+  opendatatoronto::get_resource()
+  
+#### Save data ### 
+write_csv(all_data, "inputs/data/data_business_licences.csv")
